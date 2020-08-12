0fd5aef2a3cccf249c62e22e3c4591f9a3aec966
Dan Fellner <dfellner92@gmail.com>
2020-08-11 20:35:17 -0400

    foundation

---
+++ models/book.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const blogPostSchema = new Schema({
+  title: { type: String, required: true},
+  description: {type: String, required: true},
+  date: { type: Date, default: Date.now }
+});
+
+const BlogPost = mongoose.model("Blog Post", blogPostSchema);
+
+module.exports = BlogPost;
---
+++ routes/api/books.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// Matches with "/api/books"
+router.route("/")
+  .post(booksController.create)
+  .get(booksController.findAll)
+
+  
+
+
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove);
+
+module.exports = router;
